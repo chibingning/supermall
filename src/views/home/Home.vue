3430c28d30d884133356f16973c2779a68f3a074
chihuimin <chibingning@foxmail.com>
2020-02-12 13:39:06 +0800

    首页网络请求

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -9,9 +9,29 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
 export default {
+    name:"home",
     components:{
         NavBar
+    },
+    data(){
+        return{
+            banner:[],
+            recommend:[],
+            dKeyword:[],
+            keywords:[]
+        }
+    },
+    created(){
+        getHomeMultidata().then(res=>{
+
+            this.banner = res.data.banner;
+            this.recommend = res.data.recommend;
+            this.dKeyword = res.data.dKeyword;
+            this.keywords = res.data.keywords;
+
+        })
     }
 }
 </script>
