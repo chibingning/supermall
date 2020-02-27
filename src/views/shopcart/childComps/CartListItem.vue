b385e88272e247d08b9bdf690713dc816811c914
chihuimin <chibingning@foxmail.com>
2020-02-27 18:50:07 +0800

    购物车选中

--- src/views/shopcart/childComps/CartListItem.vue
+++ src/views/shopcart/childComps/CartListItem.vue
@@ -1,10 +1,10 @@
 <template>
-<div id="shop-item">
+  <div id="shop-item">
     <div class="item-selector">
-
+        <check-btn :is-checked="item.checked" @click.native="checkClick"/>
     </div>
     <div class="item-img">
-      <img :src="item.image" alt="商品图片">
+      <img :src="item.image" alt="商品图片" />
     </div>
     <div class="item-info">
       <div class="item-title">{{item.title}}</div>
@@ -18,76 +18,86 @@
 </template>
 
 <script>
+import CheckBtn from './CheckBtn'
 export default {
-    name:'CartListItem',
-    props:{
-        item:{
-            type:Object,
-            default(){
-                return {}
-            }
-        }
+  name: "CartListItem",
+  components:{
+      CheckBtn
+  },
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {};
+      }
     }
-}
+  },
+  methods:{
+      checkClick(){
+          this.item.checked = !this.item.checked
+      }
+  }
+};
 </script>
 
 <style scoped>
- #shop-item {
-    width: 100%;
-    display: flex;
-    font-size: 0;
-    padding: 5px;
-    border-bottom: 1px solid #ccc;
-  }
+#shop-item {
+  width: 100%;
+  display: flex;
+  font-size: 0;
+  padding: 5px;
+  border-bottom: 1px solid #ccc;
+}
 
-  .item-selector {
-    width: 14%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.item-selector {
+  width: 14%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .item-title, .item-desc {
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-  }
+.item-title,
+.item-desc {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
 
-  .item-img {
-    padding: 5px;
-    /*border: 1px solid #ccc;*/
-  }
+.item-img {
+  padding: 5px;
+  /*border: 1px solid #ccc;*/
+}
 
-  .item-img img {
-    width: 80px;
-    height: 100px;
-    display: block;
-    border-radius: 5px;
-  }
+.item-img img {
+  width: 80px;
+  height: 100px;
+  display: block;
+  border-radius: 5px;
+}
 
-  .item-info {
-    font-size: 17px;
-    color: #333;
-    padding: 5px 10px;
-    position: relative;
-    overflow: hidden;
-  }
+.item-info {
+  font-size: 17px;
+  color: #333;
+  padding: 5px 10px;
+  position: relative;
+  overflow: hidden;
+}
 
-  .item-info .item-desc {
-    font-size: 14px;
-    color: #666;
-    margin-top: 15px;
-  }
+.item-info .item-desc {
+  font-size: 14px;
+  color: #666;
+  /* margin-top: 15px; */
+}
 
-  .info-bottom {
-    margin-top: 10px;
-    position: absolute;
-    bottom: 10px;
-    left: 10px;
-    right: 10px;
-  }
+.info-bottom {
+  margin-top: 10px;
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  right: 10px;
+}
 
-  .info-bottom .item-price {
-    color: orangered;
-  }
+.info-bottom .item-price {
+  color: orangered;
+}
 </style>
---
+++ src/views/shopcart/childComps/CheckBtn.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="check" :class="{checked:isChecked}">
+
+</div>
+</template>
+
+<script>
+export default{
+    props:{
+        isChecked:{
+            type:Boolean,
+            default(){
+                return false
+            }
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+.check{ width:20px; height: 20px; margin-right:5px; border-radius:50%; background: url(~assets/img/shopcart/check.svg)no-repeat; background-size:cover}
+.checked{ width:20px; height: 20px; margin-right:5px; border-radius:50%; background: url(~assets/img/shopcart/checked.svg)no-repeat; background-size:cover}
+
+</style>
